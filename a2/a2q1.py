4c19dc106be4cc138559fbe8d83949772ede5c1c
Germ <jeremythegeek@gmail.com>
2020-07-08 11:52:15 -0600

    tidy submit

--- a2/a2q1.py
+++ a2/a2q1.py
@@ -1,4 +1,5 @@
 # Jeremy Bernhardt, jrb569, 11119747, CMPT 145: 2020ST2
+# a2q1: Module for processing walks
 
 import sys
 from typing import List, Tuple, Dict
@@ -45,11 +46,22 @@ if __name__ == '__main__':
     # define axis to use
     axis: List[Tuple[str, str]] = [('L', 'R')]
 
+    # Loop over system args
     for fName in sys.argv[1:]:
+        # Attempt open, exit on failure
         try:
             with open(fName) as f:
+                print(f"Processing: {fName}")
+                # Pass off each line to be calculated, display output
                 for line in f.readlines():
-                    print(calcWalk(line, axis))
+                    # Display in cannonical way for single pairs
+                    if len(axis) == 1:
+                        for v in calcWalk(line, axis).values():
+                            print(v)
+
+                    # verbose output for multi-axis
+                    else:
+                        print(calcWalk(line, axis))
 
         except OSError:
             print(f"Could not open file {fName}, exiting.")
--- a2/test_a2q1.py
+++ a2/test_a2q1.py
@@ -1,15 +1,19 @@
 import unittest
 import a2q1
 
+# Test harness for walkers
 class TestCalcWalk(unittest.TestCase):
-    testData = [  ['L L L', 3],   ['R L L', 1],
-                  ['R R L', 1],   ['R R R', 3],
-                  ['R L L R', 0], ['L R R L R', 1]]
+    testData = [  ['L L L', 3],             ['R L L', 1],
+                  ['R R L', 1],             ['R R R', 3],
+                  ['asdfsadseesdf', 0],     ['U', 0],
+                  ['R L L R', 0],           ['L R R L R', 1]]
 
+    # verify a single pair
     def test_single_walk(self):
         for inp, out in self.testData:
             self.assertEqual(a2q1.getDistance(inp, ('L', 'R')), out)
 
+    # verify list of pairs
     def test_double_walk(self):
         pairs = [('L', 'R'), ('U', 'D')]
         out = {"U/D": 0, "L/R": 0}
